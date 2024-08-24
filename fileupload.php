1ac8d7ca58f4f9c668bcceef517e36a680dd7ce2
personal2private <124185511+personal2private@users.noreply.github.com>
2024-08-24 09:59:20 +0530

    fileupload.php

--- fileupload.php
+++ fileupload.php
@@ -1,6 +1,6 @@
 
 <?php
-$target_dir = "uploads/";
+$target_dir = "https://github.com/personal2private/webtest/tree/main/uploads";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
